79bcb868f6eb1a880559a7b238b8c5af1421c078
Wander Madeira <wandermadeirasilva2@gmail.com>
2026-02-12 11:30:56 +0000

    Atualizar o WebAgentApplicationTests.java

--- src/test/java/com/homelab/web_agent/WebAgentApplicationTests.java
+++ src/test/java/com/homelab/web_agent/WebAgentApplicationTests.java
@@ -3,6 +3,7 @@ package com.homelab.web_agent;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//basic annotatiob
 @SpringBootTest
 class WebAgentApplicationTests {
 
